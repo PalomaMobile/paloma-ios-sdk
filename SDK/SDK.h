a4efa10ab35e32225382ecfab0675c0b9d0ad8d5
Karel Herink <karel.herink@gmail.com>
2015-11-27 14:25:02 +1100

    Initial Commit

---
+++ SDK/SDK.h
@@ -0,0 +1,19 @@
+//
+//  SDK.h
+//  SDK
+//
+//  Created by karel herink on 27/11/2015.
+//  Copyright © 2015 Paloma Mobile Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDK.
+FOUNDATION_EXPORT double SDKVersionNumber;
+
+//! Project version string for SDK.
+FOUNDATION_EXPORT const unsigned char SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK/PublicHeader.h>
+
+
